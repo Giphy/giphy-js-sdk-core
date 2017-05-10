de9987dc687fb49246f0da8748524e57bfb33b61
Cosmo Cochrane <cosmocochrane@Cosmos-MacBook-Pro-2.local>
2017-05-10 11:18:42 -0400

    refactored Media.js to switch case

--- src/utils/Media.js
+++ src/utils/Media.js
@@ -16,24 +16,65 @@ function Media(data, endpoint) {
   String webp;
   String webp_size;
   */
-  //MANIPULATE FORMAT HERE IF NECESSARY
-  data.username
-  
-  if(endpoint === 'random'){
-    modifiedData = {};
-    modifiedData["images"] = Images(imagesFormatter(data), data.id, true);
-    modifiedData.user = {username: data.username};
-    modifiedData.url = data.url;
-    modifiedData.type = data.type;
-    return modifiedData;
-  } else {
-    delete data.username;
-    if (data["images"]) {
-      data["images"] = Images(data["images"], data.id)
-    } else if (data["user"]) {
-      data["user"] = User(data["user"])
-    }
-    return data
+  //MANIPULATE FORMAT HERE IF NECESSARY  
+  switch(endpoint) {
+  case("random"):
+    return {
+      images: {
+        fixed_height_downsampled: {
+          url: data.fixed_height_downsampled_url,
+          height: data.fixed_height_downsampled_height,
+          width: data.fixed_height_downsampled_width
+        },
+        fixed_height_small: {
+          url: data.fixed_height_small_url,
+          height: data.fixed_height_small_height,
+          width: data.fixed_height_small_width
+        },
+        fixed_width_downsampled: {
+          url: data.fixed_height_small_url,
+          height: data.fixed_height_small_height,
+          width: data.fixed_height_small_width
+        },
+        fixed_height_downsampled: {
+          url: data.fixed_height_small_url,
+          height: data.fixed_height_small_height,
+          width: data.fixed_height_small_width
+        },
+        fixed_width_small: {
+          url: data.fixed_width_small_url,
+          height: data.fixed_width_small_height,
+          width: data.fixed_width_small_width
+        },
+        fixed_width_small_still: {
+          url: data.fixed_width_small_url,
+          height: data.fixed_width_small_height,
+          width: data.fixed_width_small_width
+        },
+        original: {
+          frames: data.image_frames,
+          url: data.image_original_url,
+          height: data.image_height,
+          mp4: data.image_mp4_url,
+          width: data.image_width
+        },
+        id: data.id;
+      },
+      user: {
+        username: data.username
+      },
+      url: data.url,
+      type: data.type
+    };
+    case "trending":
+    case "translate":
+    case "search":
+      delete data.username;
+      data.images.id = data.id;
+      return data
+
+    default:
+      throw "Unimplemented endpoint " + endpoint
   }
 
 }
