4d1461f3e3c463cdb288d3559c8385fb9dce42ce
Jeff Pearsall <jeffpearsall@gmail.com>
2018-07-10 12:41:02 -0700

    removing _url so sdk will return the webp image instead of null (#25)

--- src/utils/Image.js
+++ src/utils/Image.js
@@ -18,7 +18,7 @@ var Image = function(data, id, rendition_type) {
     frames: data.frames ? data.frames : null,
     mp4_url: data.mp4 ? data.mp4 : null,
     mp4_size: data.mp4_size ? data.mp4_size : null,
-    webp_url: data.webp_url ? data.webp_url : null,
+    webp_url: data.webp ? data.webp : null,
     webp_size: data.webp_size ? data.webp_size : null
   })
 }
